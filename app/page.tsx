4aaca5d2e03a582b3e2c30690728775f9324cb8e
mridu-pawan <mridupawanbaruah811@gmail.com>
2026-02-05 13:36:20 +0530

    database-init

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany();
+
   return (
     <div className="flex min-h-screen min-w-screen items-center justify-center">
+      <div>{JSON.stringify(users)}</div>
       <Button variant={"outline"}>Click me</Button>
     </div>
   );
