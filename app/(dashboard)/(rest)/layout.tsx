aac8ef2b496f9f647f791b69bf5bbaad189723ea
mridu-pawan <mridupawanbaruah811@gmail.com>
2026-02-07 17:25:10 +0530

    sidebar-layout

---
+++ app/(dashboard)/(rest)/layout.tsx
@@ -0,0 +1,18 @@
+import AppHeader from "@/components/app-header";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Sidebar,
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <AppHeader />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
