613b5f64b00a7b92828a7aa84525550d459c8e59
mridu-pawan <mridupawanbaruah811@gmail.com>
2026-02-11 22:14:58 +0530

    node-selector

--- components/initial-node.tsx
+++ components/initial-node.tsx
@@ -2,19 +2,24 @@
 
 import type { NodeProps } from "@xyflow/react";
 import { PlusIcon } from "lucide-react";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { PlaceholderNode } from "./react-flow/placeholder-node";
 import { WorkflowNode } from "./workflow-node";
+import { NodeSelector } from "./node-selector";
 
 export const InitialNode = memo((props: NodeProps) => {
+  const [selectorOpen, setSelectorOpen] = useState(false);
+
   return (
-    <WorkflowNode name="Initial Node" description="Click to add a node">
-      <PlaceholderNode {...props} onClick={() => {}}>
-        <div className="flex cursor-pointer items-center justify-center">
-          <PlusIcon className="size-4" />
-        </div>
-      </PlaceholderNode>
-    </WorkflowNode>
+    <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
+      <WorkflowNode name="Initial Node" description="Click to add a node">
+        <PlaceholderNode {...props} onClick={() => setSelectorOpen(true)}>
+          <div className="flex cursor-pointer items-center justify-center">
+            <PlusIcon className="size-4" />
+          </div>
+        </PlaceholderNode>
+      </WorkflowNode>
+    </NodeSelector>
   );
 });
 
