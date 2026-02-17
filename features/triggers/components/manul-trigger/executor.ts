87c62aec8ba435b7c1c24db159cfcaf793cb4903
mridu-pawan <mridupawanbaruah811@gmail.com>
2026-02-17 18:16:15 +0530

    inngest/realtime-nodeStatus-ui/ux

--- features/triggers/components/manul-trigger/executor.ts
+++ features/triggers/components/manul-trigger/executor.ts
@@ -1,4 +1,5 @@
 import type { NodeExecutor } from "@/features/executions/lib/types";
+import { manualTriggerChannel } from "@/inngest/channels/manul-trigger";
 
 type ManualTriggerData = Record<string, unknown>;
 
@@ -6,12 +7,23 @@ export const manualTriggerExecutor: NodeExecutor<ManualTriggerData> = async ({
   nodeId,
   context,
   step,
+  publish,
 }) => {
-  //TODO: Publish "loading" state for manual trigger
+  await publish(
+    manualTriggerChannel().status({
+      nodeId,
+      status: "loading",
+    }),
+  );
 
   const result = await step.run("manul-trigger", async () => context);
 
-  //TODO: Publish "success" state for manual trigger
+  await publish(
+    manualTriggerChannel().status({
+      nodeId,
+      status: "success",
+    }),
+  );
 
   return result;
 };
--- features/triggers/components/manul-trigger/node.tsx
+++ features/triggers/components/manul-trigger/node.tsx
@@ -3,10 +3,18 @@ import { memo, useState } from "react";
 import { BaseTriggerNode } from "../base-trigger-node";
 import { MousePointerIcon } from "lucide-react";
 import { ManualTriggerDialog } from "./dialog";
+import { UseNodeStatus } from "@/features/executions/hooks/use-node-status";
+import { MANUAL_TRIGGER_CHANNEL_NAME } from "@/inngest/channels/manul-trigger";
+import { fetchManualRealtimeToken } from "./actions";
 
 export const ManualTriggerNode = memo((props: NodeProps) => {
   const [dialogOpen, setDialogOpen] = useState(false);
-  const nodeStatus = "initial";
+  const nodeStatus = UseNodeStatus({
+    nodeId: props.id,
+    channel: MANUAL_TRIGGER_CHANNEL_NAME,
+    topic: "status",
+    refreshToken: fetchManualRealtimeToken,
+  });
 
   const handleOpenSettings = () => setDialogOpen(true);
 
